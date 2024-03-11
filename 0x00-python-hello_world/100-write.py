eefdfa4210f2c1ad314d9586cc341d09e388157c
Rodas Getachew <getachewrodas39@gmail.com>
2024-03-11 23:22:47 +0300

    updated

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 import sys
-string = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-sys.stderr.write(string)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
