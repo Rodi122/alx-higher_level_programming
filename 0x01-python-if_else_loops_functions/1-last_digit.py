91085661def60e39f029768be9c53cb70b44d728
Rodas Getachew <getachewrodas39@gmail.com>
2024-03-12 18:27:12 +0300

    edited task1's answer

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,17 +2,17 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-lastDigitVal = number % 10
-lastDigitSign = 1 if number > 0 else -1
+last_digit_sign = 1 if number > 0 else -1
+last_digit_val = number % 10 if last_digit_sign == 1 else number % -10
 
-lastDigit = lastDigitVal * lastDigitSign
-returnString = "Last digit of " + str(number) + " is " + str(lastDigit)
+last_digit = last_digit_val * last_digit_sign
+return_string = "Last digit of " + str(number) + " is " + str(last_digit)
 
-if (lastDigit > 5):
-    returnString += " and is greater than 5"
-elif(lastDigit == 0):
-    returnString += " and is 0"
-elif(lastDigit < 6 and lastDigit != 0):
-    returnString += " and is less than 6 and not 0"
+if (last_digit > 5):
+    return_string = return_string + " and is greater than 5"
+elif (last_digit == 0):
+    return_string = return_string + " and is 0"
+elif (last_digit < 6 and last_digit != 0):
+    return_string = return_string + " and is less than 6 and not 0"
 
-print(returnString)
+print(return_string)
